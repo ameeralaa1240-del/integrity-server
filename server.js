32903539110c1c83e0f1e552c86d54f0aaf62e47
ameeralaa1240-del <ameeralaa1240@gmail.com>
2026-09-02 05:11:17 +0300

    Change server port from 10000 to 8080

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
 });
 
 const ADMIN_TOKEN = "medo123";
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT ||  8080;
 
 let data = {
     username: "في انتظار البيانات",
